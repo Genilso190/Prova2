37b43152fff534028b789b5804d961113b2bd8b2
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-06-18 23:33:26 +0000

    fix: page

--- app/admin/laptop/page.tsx
+++ app/admin/laptop/page.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
-import ListBook from "./List";
+import ListLaptop from "./List";
 
-export default function Boock() {
+export default function Laptop() {
     return (
         <div className="w-full flex flex-col  mt-6">
             <div className="flex justify-center mb-6">
-                <a href="/admin/books/new">
+                <a href="/admin/laptop/new">
                     <Button>Cadastrar Livro</Button>
                 </a>
             </div>
-            <ListBook />
+            <ListLaptop />
         </div>
     )
 }
