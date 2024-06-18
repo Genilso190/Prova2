4b16db9d086c98e33273d479f9398de28fb6d48b
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-06-18 23:32:06 +0000

    fix: list

--- app/admin/laptop/List.tsx
+++ app/admin/laptop/List.tsx
@@ -17,34 +17,34 @@ import { revalidatePath } from "next/cache"
     description: string,
   }
   
-  export default async function ListBook() {
-    const books = await list()
+  export default async function ListLaptop() {
+    const laptops = await list()
     async function list(){
-      revalidatePath("/admin/books")
-      const response = await fetch("https://server20241-beige.vercel.app/books")
+      revalidatePath("/admin/laptop")
+      const response = await fetch("https://server20241-beige.vercel.app/laptops")
         return response.json();
     }
 
-    async function deleteBook(formData: FormData) {
+    async function deleteLaptop(formData: FormData) {
       "use server"
       const id = formData.get("id") as string;
-      const response = await fetch("https://server20241-beige.vercel.app/books/"+id, {method: "DELETE"});
-      revalidatePath("/admin/books")
+      const response = await fetch("https://server20241-beige.vercel.app/laptops/"+id, {method: "DELETE"});
+      revalidatePath("/admin/laptop")
   
     }
 
     return (
       <Table>
-        <TableCaption>Lista de Livro</TableCaption>
+        <TableCaption>Lista de computadores</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Titulo</TableHead>
+            <TableHead>Modelo</TableHead>
             <TableHead>Descrição</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {books.map((item:IBook) => (
+          {laptops.map((item:ILaptop) => (
             <TableRow key={item.id}>
               <TableCell className="font-medium">{item.id}</TableCell>
               <TableCell>{item.title}</TableCell>
@@ -52,7 +52,7 @@ import { revalidatePath } from "next/cache"
               <TableCell>
               <form>
                 <input type="text" hidden title="id" value={item.id} />
-                <Button variant="destructive" formAction={deleteBook}>EXCLUIR</Button>
+                <Button variant="destructive" formAction={deleteLaptop}>EXCLUIR</Button>
               </form>
             </TableCell>
             </TableRow>
